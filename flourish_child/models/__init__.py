8f380f4a1e061429390e9000cd810427ea9812b9
ame diphoko <diphokoame@gmail.com>
2020-11-23 08:09:14 +0200

    update choices

--- flourish_child/models/__init__.py
+++ flourish_child/models/__init__.py
@@ -1,15 +1,19 @@
 from .academic_performance import AcademicPerformance
 from .birth_data import BirthData
+from .birth_feeding_and_vaccine import BirthFeedingVaccine, BirthVaccines
 from .birth_exam import BirthExam
 from .child_birth_weight_length_screening import ChildBirthScreening
 from .child_clinical_measurements import ChildClinicalMeasurements
 from .child_dataset import ChildDataset
 from .child_socio_demographic import ChildSocioDemographic
+from .child_gad_anxiety_screening import ChildGadAnxietyScreening
 from .child_immunization_history import ChildImmunizationHistory
 from .child_immunization_history import VaccinesReceived
 from .child_immunization_history import VaccinesMissed
+from .child_phq_depression_screening import ChildPhqDepressionScreening
 from .child_referral import ChildReferral
 from .child_visit import ChildVisit
+from .infant_arv_exposure import InfantArvExposure
 from .infant_congenital_anomalies import InfantCardioDisorder, InfantFacialDefect
 from .infant_congenital_anomalies import InfantCleftDisorder, InfantMouthUpGi, InfantCns
 from .infant_congenital_anomalies import InfantCongenitalAnomalies, BaseCnsItem
