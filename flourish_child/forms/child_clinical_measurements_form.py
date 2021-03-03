ad23d7afdea9fd91b6c120b2bfaecf9f69a1cc96
ame diphoko <diphokoame@gmail.com>
2021-03-03 23:42:04 +0200

    update clinical measurements form

--- flourish_child/forms/child_clinical_measurements_form.py
+++ flourish_child/forms/child_clinical_measurements_form.py
@@ -1,10 +1,13 @@
 from django import forms
+from flourish_child_validations.form_validators import ChildClinicalMeasurementsFormValidator
 
 from .child_form_mixin import ChildModelFormMixin
 from ..models import ChildClinicalMeasurements
 
 
 class ChildClinicalMeasurementsForm(ChildModelFormMixin, forms.ModelForm):
+
+    form_validator_cls = ChildClinicalMeasurementsFormValidator
 
     class Meta:
         model = ChildClinicalMeasurements
