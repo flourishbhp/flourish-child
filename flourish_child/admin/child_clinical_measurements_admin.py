a8dc34c589a7e81ae29c7c62367609eb8c776bbe
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-04-15 12:17:24 +0200

    Update child clinical measurements model & admin

--- flourish_child/admin/child_clinical_measurements_admin.py
+++ flourish_child/admin/child_clinical_measurements_admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.core.exceptions import ObjectDoesNotExist
 from edc_model_admin import audit_fieldset_tuple
+from edc_fieldsets.fieldlist import Remove
 
 from .model_admin_mixins import ChildCrfModelAdminMixin
 from ..admin_site import flourish_child_admin
@@ -8,12 +10,13 @@ from ..models import ChildClinicalMeasurements
 
 
 @admin.register(ChildClinicalMeasurements, site=flourish_child_admin)
-class ChildClinicalMeasurementsAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
-
+class ChildClinicalMeasurementsAdmin(ChildCrfModelAdminMixin,
+                                     admin.ModelAdmin):
     form = ChildClinicalMeasurementsForm
 
     list_display = ('child_visit', 'child_weight_kg', 'child_height',
-                    'child_systolic_bp', 'child_diastolic_bp', 'child_waist_circ',
+                    'child_systolic_bp', 'child_diastolic_bp',
+                    'child_waist_circ',
                     'child_hip_circ')
 
     fieldsets = (
@@ -33,3 +36,19 @@ class ChildClinicalMeasurementsAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
                 'child_muac'
             ]}
          ), audit_fieldset_tuple)
+
+    conditional_fieldlists = {}
+
+    def get_key(self, request, obj=None):
+        try:
+            appt_obj = self.get_instance(request)
+        except ObjectDoesNotExist:
+            return None
+        else:
+            return appt_obj.visit_code
+
+    conditional_fieldlists.update(
+        {'1000':
+         Remove('skin_folds_triceps',
+                'skin_folds_subscapular',
+                'skin_folds_suprailiac'), })
