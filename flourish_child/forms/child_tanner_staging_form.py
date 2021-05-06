4c42edcecfab474bf1a11b796a8914b2a2995e29
ame diphoko <diphokoame@gmail.com>
2021-05-06 19:16:06 +0200

    add initial value for gender, child tanner staging form

--- flourish_child/forms/child_tanner_staging_form.py
+++ flourish_child/forms/child_tanner_staging_form.py
@@ -2,12 +2,26 @@ from django import forms
 from flourish_child_validations.form_validators import ChildTannerStagingFormValidator
 
 from .child_form_mixin import ChildModelFormMixin
-from ..models import ChildTannerStaging
+from ..models import ChildTannerStaging, ChildAssent
 
 
 class ChildTannerStagingForm(ChildModelFormMixin, forms.ModelForm):
 
     form_validator_cls = ChildTannerStagingFormValidator
+
+    def __init__(self, *args, **kwargs):
+        initial = kwargs.pop('initial', {})
+        subject_identifier = initial.get('subject_identifier')
+        if subject_identifier:
+            try:
+                assent = ChildAssent.objects.get(
+                    subject_identifier=subject_identifier)
+            except ChildAssent.DoesNotExist:
+                return None
+            else:
+                initial['child_gender'] = getattr(assent, 'gender')
+        kwargs['initial'] = initial
+        super().__init__(*args, **kwargs)
 
     class Meta:
         model = ChildTannerStaging
