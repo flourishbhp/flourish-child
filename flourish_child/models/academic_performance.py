c84531d7d9f77720a00df8273916ed0e7e256add
ame diphoko <diphokoame@gmail.com>
2021-05-06 19:43:18 +0200

    require all grading questions, academic performance

--- flourish_child/models/academic_performance.py
+++ flourish_child/models/academic_performance.py
@@ -14,100 +14,85 @@ class AcademicPerformance(ChildCrfModelMixin):
 
     education_level_other = OtherCharField(
         verbose_name='Specify other',
-        blank=True,
-        null=True,
         max_length=30)
 
     mathematics_marks = models.CharField(
         verbose_name='What are your marks in Mathematics?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     science_marks = models.CharField(
         verbose_name='What are your marks in Science?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     setswana_marks = models.CharField(
         verbose_name='What are your marks in Setswana?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     english_marks = models.CharField(
         verbose_name='What are your marks in English?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     physical_edu_marks = models.CharField(
         verbose_name='What are your marks in Physical Education?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     cultural_stds_marks = models.CharField(
         verbose_name='What are your marks in Cultural Studies?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     social_stds_marks = models.CharField(
         verbose_name='What are your marks in Social Studies?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     agriculture_marks = models.CharField(
         verbose_name='What are your marks in Agriculture?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     single_scie_marks = models.CharField(
         verbose_name='What are your marks in Single Science?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     biology_marks = models.CharField(
         verbose_name='What are your marks in Biology?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     chemistry_marks = models.CharField(
         verbose_name='What are your marks in Chemistry?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     physics_marks = models.CharField(
         verbose_name='What are your marks in Physics?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     double_scie_marks = models.CharField(
         verbose_name='What are your marks in Double Science?',
         max_length=20,
         choices=MARKS,
-        blank=True,
-        null=True,)
+        default='not_taking_subject')
 
     overall_performance = models.CharField(
         verbose_name='What is your overall performance in your last examination?',
