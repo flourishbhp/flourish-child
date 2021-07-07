48037675d0c02603d28adca92a0ea8494ed6819a
tumiemosweu <imosweuk@gmail.com>
2021-07-07 17:04:10 +0200

    update child appointment form

--- flourish_child/forms/child_appointment_form.py
+++ flourish_child/forms/child_appointment_form.py
@@ -15,6 +15,25 @@ class AppointmentForm(SiteModelFormMixin, FormValidatorMixin, forms.ModelForm):
 
     form_validator_cls = AppointmentFormValidator
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        if cleaned_data.get('appt_datetime'):
+
+            visit_definition = self.instance.visits.get(self.instance.visit_code)
+
+            earlist_appt_date = (self.instance.timepoint_opened_datetime -
+                                 visit_definition.rlower)
+            latest_appt_date = (self.instance.timepoint_opened_datetime +
+                                visit_definition.rupper)
+
+            if (cleaned_data.get('appt_datetime') < earlist_appt_date
+                    or cleaned_data.get('appt_datetime') > latest_appt_date):
+                raise forms.ValidationError(
+                            'The appointment datetime cannot be outside the window period, '
+                            'please correct. See earliest, ideal and latest datetimes below.')
+        super().clean()
+
     class Meta:
         model = Appointment
         fields = '__all__'
