26df72bbca0ec4e29ea589fad19395eb9c343df4
Itumeleng Mosweu <imosweuk@gmail.com>
2021-07-27 16:50:25 +0200

    Revert "remove unused code"
    This reverts commit e71a9a90078593e47f3761c479a2eeffbae3fbe7.

--- flourish_child/models/birth_data.py
+++ flourish_child/models/birth_data.py
@@ -3,6 +3,7 @@ from django.db import models
 from edc_constants.choices import YES_NO, GENDER
 from edc_constants.constants import YES
 
+
 from .child_crf_model_mixin import ChildCrfModelMixin
 
 
@@ -12,7 +13,8 @@ class BirthData(ChildCrfModelMixin):
     infant_gender = models.CharField(
         max_length=6,
         choices=GENDER,
-        verbose_name="What is the gender of the infant?")
+        verbose_name="What is the gender of the infant?",
+        help_text="")
 
     weight_avail = models.CharField(
         max_length=3,
