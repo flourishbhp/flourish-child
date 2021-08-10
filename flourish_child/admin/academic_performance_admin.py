743d7bf03dad6d7a6d565f12dd0c4227aa19fe85
Itumeleng Mosweu <imosweuk@gmail.com>
2021-08-10 11:28:19 +0200

    add stem question to academic performance

--- flourish_child/admin/academic_performance_admin.py
+++ flourish_child/admin/academic_performance_admin.py
@@ -1,11 +1,14 @@
 from django.apps import apps as django_apps
 from django.contrib import admin
 from edc_fieldsets.fieldlist import Insert
+from edc_fieldsets.fieldsets import Fieldsets
+from edc_fieldsets.fieldsets_modeladmin_mixin import FormLabel
 from edc_model_admin import audit_fieldset_tuple
 from ..admin_site import flourish_child_admin
 from ..forms import AcademicPerformanceForm
 from ..models import AcademicPerformance
 from .model_admin_mixins import ChildCrfModelAdminMixin
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @admin.register(AcademicPerformance, site=flourish_child_admin)
@@ -41,7 +44,8 @@ class AcademicPerformanceAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
         'physics_marks': admin.VERTICAL,
         'double_scie_marks': admin.VERTICAL,
         'overall_performance': admin.VERTICAL,
-        'num_days': admin.VERTICAL, }
+        'num_days': admin.VERTICAL,
+        'academic_perf_changed': admin.VERTICAL}
 
     conditional_fieldlists = {
         'primary': Insert('mathematics_marks',
@@ -68,6 +72,60 @@ class AcademicPerformanceAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
                          after='education_level'),
         }
 
+    def get_fieldsets(self, request, obj=None):
+        """Returns fieldsets after modifications declared in
+        "conditional" dictionaries.
+        """
+        fieldsets = super().get_fieldsets(request, obj=obj)
+        fieldsets = Fieldsets(fieldsets=fieldsets)
+        keys = self.get_keys(request, obj)
+        for key in keys:
+            fieldset = self.conditional_fieldsets.get(key)
+            if fieldset:
+                try:
+                    fieldset = tuple(fieldset)
+                except TypeError:
+                    fieldset = (fieldset,)
+                for f in fieldset:
+                    fieldsets.add_fieldset(fieldset=f)
+            fieldlist = self.conditional_fieldlists.get(key)
+            if fieldlist:
+                try:
+                    fieldsets.insert_fields(
+                        *fieldlist.insert_fields,
+                        insert_after=fieldlist.insert_after,
+                        section=fieldlist.section)
+                except AttributeError:
+                    pass
+                try:
+                    fieldsets.remove_fields(
+                        *fieldlist.remove_fields,
+                        section=fieldlist.section)
+                except AttributeError:
+                    pass
+        fieldsets = self.update_fieldset_for_form(
+            fieldsets, request)
+        fieldsets.move_to_end(self.fieldsets_move_to_end)
+        return fieldsets.fieldsets
+
+    custom_form_labels = [
+        FormLabel(
+            field='academic_perf_changed',
+            label=('Since the last scheduled visit in {previous}, has any of your subject '
+                   'marks or overall performance in your last examination changed?'),
+            previous_appointment=True)
+        ]
+
+    quartely_schedules = ['child_a_quart_schedule1', 'child_a_fu_quart_schedule1',
+                          'child_a_sec_schedule1', 'child_b_quart_schedule1',
+                          'child_b_fu_quart_schedule1', 'child_b_sec_schedule1',
+                          'child_c_quart_schedule1', 'child_c_fu_quart_schedule1',
+                          'child_c_sec_schedule1', ]
+
+    for schedule in quartely_schedules:
+        conditional_fieldlists.update(
+            {schedule: Insert('academic_perf_changed', after='report_datetime')})
+
     def get_socio_demographic_object(self, request):
         socio_demographic_cls = django_apps.get_model('flourish_child.childsociodemographic')
 
@@ -85,17 +143,32 @@ class AcademicPerformanceAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
                 return socio_demographic_obj
 
     def get_key(self, request, obj=None):
+        pass
+
+    def get_keys(self, request, obj=None):
         socio_demographic_obj = self.get_socio_demographic_object(request)
+
+        keys = []
+        if self.get_previous_instance(request):
+            try:
+                model_obj = self.get_instance(request)
+            except ObjectDoesNotExist:
+                pass
+            else:
+                schedule_name = model_obj.schedule_name
+                keys.append(schedule_name)
 
         if socio_demographic_obj:
             education_level = (socio_demographic_obj.education_level
                                or socio_demographic_obj.education_level_other)
+
             if 'standard' in socio_demographic_obj.education_level:
-                return 'primary'
+                keys.append('primary')
             elif education_level in ['form_1', 'form_2', 'form_3']:
-                return 'junior'
+                keys.append('junior')
             elif education_level in ['form_4', 'form_5']:
-                return 'senior'
+                keys.append('senior')
+        return keys
 
     def add_view(self, request, form_url='', extra_context=None):
         socio_demographic_obj = self.get_socio_demographic_object(request)
@@ -109,3 +182,9 @@ class AcademicPerformanceAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
             request.GET = g
 
         return super().add_view(request, form_url, extra_context)
+
+    def get_form(self, request, obj=None, *args, **kwargs):
+        form = super().get_form(request, *args, **kwargs)
+        form.previous_instance = self.get_previous_instance(request)
+        return form
+
--- flourish_child/forms/academic_performance_form.py
+++ flourish_child/forms/academic_performance_form.py
@@ -1,6 +1,8 @@
 from django import forms
-from flourish_child_validations.form_validators import \
-    AcademicPerformanceFormValidator
+from django.db.models import ManyToManyField
+from flourish_child_validations.form_validators import AcademicPerformanceFormValidator
+from itertools import chain
+from edc_constants.constants import YES, NO
 from ..models import AcademicPerformance
 from .child_form_mixin import ChildModelFormMixin
 
@@ -12,6 +14,65 @@ class AcademicPerformanceForm(ChildModelFormMixin):
     education_level = forms.CharField(
         label='What level/class of school is the child currently in?',
         widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+
+    def __init__(self, *args, **kwargs):
+        initial = kwargs.pop('initial', {})
+        instance = kwargs.get('instance')
+        previous_instance = getattr(self, 'previous_instance', None)
+
+        if not instance and previous_instance:
+            for key in self.base_fields.keys():
+                if key not in ['child_visit', 'report_datetime']:
+                    initial[key] = getattr(previous_instance, key)
+        kwargs['initial'] = initial
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        previous_instance = getattr(self, 'previous_instance', None)
+        has_changed = self.compare_instance_fields(previous_instance)
+
+        academic_perf_changed = self.cleaned_data.get('academic_perf_changed')
+        if academic_perf_changed:
+            if academic_perf_changed == YES and not has_changed:
+                message = {'academic_perf_changed':
+                           'Participant\'s Socio-demographic information has changed since '
+                           'last visit. Please update the information on this form.'}
+                raise forms.ValidationError(message)
+            elif academic_perf_changed == NO and has_changed:
+                message = {'academic_perf_changed':
+                           'Participant\'s Socio-demographic information has not changed '
+                           'since last visit. Please don\'t make any changes to this form.'}
+                raise forms.ValidationError(message)
+        cleaned_data = super().clean()
+        return cleaned_data
+
+    def compare_instance_fields(self, prev_instance=None):
+        exclude_fields = ['modified', 'created', 'user_created', 'user_modified',
+                          'hostname_created', 'hostname_modified', 'device_created',
+                          'device_modified', 'report_datetime', 'child_visit',
+                          'academic_perf_changed', ]
+        if prev_instance:
+            other_values = self.model_to_dict(prev_instance, exclude=exclude_fields)
+            values = {key: self.data.get(key) or 'not_taking_subject' for
+                      key in other_values.keys()}
+            if self.data.get('grade_points') is None:
+                values['grade_points'] = None
+            return values != other_values
+        return False
+
+    def model_to_dict(self, instance, exclude):
+        opts = instance._meta
+        data = {}
+        for f in chain(opts.concrete_fields, opts.private_fields, opts.many_to_many):
+            if not getattr(f, 'editable', False):
+                continue
+            if exclude and f.name in exclude:
+                continue
+            if isinstance(f, ManyToManyField):
+                data[f.name] = [str(obj.id) for obj in f.value_from_object(instance)]
+                continue
+            data[f.name] = f.value_from_object(instance) or None
+        return data
 
     class Meta:
         model = AcademicPerformance
