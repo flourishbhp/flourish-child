7e1499897fae2324440d42e664e8ee3731342ce8
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-01-13 08:46:16 +0200

    child hospitalisation form

--- flourish_child/models/__init__.py
+++ flourish_child/models/__init__.py
@@ -16,6 +16,7 @@ from .child_food_security_questionnaire import ChildFoodSecurityQuestionnaire
 from .child_gad_anxiety_screening import ChildGadAnxietyScreening
 from .child_gad_referral import ChildGadReferral
 from .child_hiv_rapid_test_counseling import ChildHIVRapidTestCounseling
+from .child_hospitalization import ChildHospitalization, AdmissionsReasons
 from .child_immunization_history import ChildImmunizationHistory
 from .child_immunization_history import VaccinesMissed
 from .child_immunization_history import VaccinesReceived
@@ -24,30 +25,40 @@ from .child_phq_depression_screening import ChildPhqDepressionScreening
 from .child_phq_referral import ChildPhqReferral
 from .child_physical_activity import ChildPhysicalActivity
 from .child_preg_testing import ChildPregTesting
-from .child_previous_hospitalization import ChildPreviousHospitalization, ChildPreHospitalizationInline
+from .child_previous_hospitalization import ChildPreviousHospitalization, \
+    ChildPreHospitalizationInline
 from .child_socio_demographic import ChildSocioDemographic
 from .child_tanner_staging import ChildTannerStaging
 from .child_visit import ChildVisit
 from .child_working_status import ChildWorkingStatus
 from .infant_arv_exposure import InfantArvExposure
-from .infant_congenital_anomalies import InfantCardioDisorder, InfantFacialDefect
-from .infant_congenital_anomalies import InfantCleftDisorder, InfantMouthUpGi, InfantCns
+from .infant_congenital_anomalies import InfantCardioDisorder, \
+    InfantFacialDefect
+from .infant_congenital_anomalies import InfantCleftDisorder, InfantMouthUpGi, \
+    InfantCns
 from .infant_congenital_anomalies import InfantCongenitalAnomalies, BaseCnsItem
-from .infant_congenital_anomalies import InfantFemaleGenital, InfantRenal, InfantTrisomies
+from .infant_congenital_anomalies import InfantFemaleGenital, InfantRenal, \
+    InfantTrisomies
 from .infant_congenital_anomalies import InfantMusculoskeletal, InfantSkin
-from .infant_congenital_anomalies import InfantOtherAbnormalityItems, InfantMaleGenital
+from .infant_congenital_anomalies import InfantOtherAbnormalityItems, \
+    InfantMaleGenital
 from .infant_congenital_anomalies import InfantRespiratoryDefect, InfantLowerGi
 from .infant_feeding import InfantFeeding
 from .infant_feeding_practices import InfantFeedingPractices
 from .list_models import *
 from .offschedule import ChildOffSchedule
-from .onschedule import OnScheduleChildCohortAEnrollment, OnScheduleChildCohortABirth
-from .onschedule import OnScheduleChildCohortAQuarterly, OnScheduleChildCohortBEnrollment
+from .onschedule import OnScheduleChildCohortAEnrollment, \
+    OnScheduleChildCohortABirth
+from .onschedule import OnScheduleChildCohortAQuarterly, \
+    OnScheduleChildCohortBEnrollment
 from .onschedule import OnScheduleChildCohortASec, OnScheduleChildCohortBSec
-from .onschedule import OnScheduleChildCohortASecQuart, OnScheduleChildCohortBSecQuart
+from .onschedule import OnScheduleChildCohortASecQuart, \
+    OnScheduleChildCohortBSecQuart
 from .onschedule import OnScheduleChildCohortBFU, OnScheduleChildCohortCFU
-from .onschedule import OnScheduleChildCohortBQuarterly, OnScheduleChildCohortCEnrollment
-from .onschedule import OnScheduleChildCohortCPool, OnScheduleChildCohortCSecQuart
+from .onschedule import OnScheduleChildCohortBQuarterly, \
+    OnScheduleChildCohortCEnrollment
+from .onschedule import OnScheduleChildCohortCPool, \
+    OnScheduleChildCohortCSecQuart
 from .onschedule import OnScheduleChildCohortCQuarterly
 from .onschedule import OnScheduleChildCohortCSec, OnScheduleChildCohortAFU
 from .signals import child_consent_on_post_save
