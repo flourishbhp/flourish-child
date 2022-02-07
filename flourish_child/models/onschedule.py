a62f73d63e987916a8a8032050d0693d75ddc30f
Itumeleng Mosweu <imosweuk@gmail.com>
2022-02-07 17:37:38 +0200

    add dummy consent v2

--- flourish_child/models/onschedule.py
+++ flourish_child/models/onschedule.py
@@ -1,10 +1,11 @@
+from edc_visit_schedule.model_mixins import OnScheduleModelMixin as BaseOnScheduleModelMixin
+
 from django.apps import apps as django_apps
 from django.db import models
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
 from edc_base.sites import CurrentSiteManager
 from edc_identifier.managers import SubjectIdentifierManager
-from edc_visit_schedule.model_mixins import OnScheduleModelMixin as BaseOnScheduleModelMixin
 
 
 class OnScheduleModelMixin(BaseOnScheduleModelMixin, BaseUuidModel):
