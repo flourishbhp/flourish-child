82f0c49e95034432aedb4ba49fd45ef098bf96d5
Moses Chawawa <moseschawawa@gmail.com>
2022-02-10 21:32:47 +0200

    prefill for child covid form

--- flourish_child/forms/child_covid_19_form.py
+++ flourish_child/forms/child_covid_19_form.py
@@ -10,24 +10,23 @@ class ChildCovid19Form(SiteModelFormMixin, FormValidatorMixin, forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         subject_identifier = self.initial.get('subject_identifier', None)
 
-        if subject_identifier:
-            covid_instance = self._last_covid_instance(subject_identifier)
+        if not subject_identifier:
+            return
 
-            self.initial['fully_vaccinated'] = covid_instance.fully_vaccinated
-            self.initial['vaccination_type'] = covid_instance.vaccination_type
-            self.initial['other_vaccination_type'] = covid_instance.other_vaccination_type
-            self.initial['first_dose'] = covid_instance.first_dose
-            self.initial['second_dose'] = covid_instance.second_dose
-
-
-
-    def _last_covid_instance(self, subject_identifier):
-        return ChildCovid19.objects \
-            .filter(maternal_visit__appointment__subject_identifier=subject_identifier) \
+        prev_instance = ChildCovid19.objects \
+            .filter(child_visit__appointment__subject_identifier=subject_identifier) \
             .order_by('-report_datetime') \
             .first()
+
+        if prev_instance:
+            self.initial['fully_vaccinated'] = prev_instance.fully_vaccinated
+            self.initial['vaccination_type'] = prev_instance.vaccination_type
+            self.initial['other_vaccination_type'] = prev_instance.other_vaccination_type
+            self.initial['first_dose'] = prev_instance.first_dose
+            self.initial['second_dose'] = prev_instance.second_dose
 
     class Meta:
         model = ChildCovid19
