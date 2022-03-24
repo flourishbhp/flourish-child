e1a2cc106a806de80acf3f0fba204bb65d38e68c
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-03-24 16:16:37 +0200

    Update child_requisition_form.py

--- flourish_child/forms/child_requisition_form.py
+++ flourish_child/forms/child_requisition_form.py
@@ -4,27 +4,21 @@ from django.conf import settings
 from django.utils import timezone
 from edc_base.utils import convert_php_dateformat
 from edc_form_validators import FormValidatorMixin
-from django import forms
-from edc_base.sites import SiteModelFormMixin
-from edc_form_validators import FormValidatorMixin
-from edc_visit_tracking.crf_date_validator import CrfDateValidator
-from edc_visit_tracking.crf_date_validator import (
-    CrfReportDateAllowanceError, CrfReportDateBeforeStudyStart)
-from edc_visit_tracking.crf_date_validator import CrfReportDateIsFuture
 from edc_lab.forms.modelform_mixins import RequisitionFormMixin
 
 from flourish_child.models.child_visit import ChildVisit
+from flourish_child_validations.form_validators import ChildRequisitionsFormValidations
 from .child_form_mixin import ChildModelFormMixin
 from ..models import ChildRequisition
 
 
 class InlineSubjectModelFormMixin(FormValidatorMixin, forms.ModelForm):
-
     visit_model = ChildVisit
 
 
 class ChildRequisitionForm(ChildModelFormMixin, RequisitionFormMixin,
                            FormValidatorMixin):
+    form_validator_cls = ChildRequisitionsFormValidations
 
     requisition_identifier = forms.CharField(
         label='Requisition identifier',
@@ -34,6 +28,8 @@ class ChildRequisitionForm(ChildModelFormMixin, RequisitionFormMixin,
         self.subject_identifier = self.cleaned_data.get(
             'child_visit').subject_identifier
         super().clean()
+
+        self.validate_specimen_drawn()
 
     def validate_requisition_datetime(self):
         requisition_datetime = self.cleaned_data.get('requisition_datetime')
@@ -46,11 +42,13 @@ class ChildRequisitionForm(ChildModelFormMixin, RequisitionFormMixin,
                     convert_php_dateformat(settings.SHORT_DATETIME_FORMAT))
                 raise forms.ValidationError({
                     'requisition_datetime':
-                    f'Invalid. Cannot be before date of visit {formatted}.'})
+                        f'Invalid. Cannot be before date of visit {formatted}.'
+                    })
 
     def validate_other_specify_field(self, form_validator=None):
         form_validator.validate_other_specify(
             field='reason_not_drawn', other_stored_value='other')
+
 
     class Meta:
         model = ChildRequisition
