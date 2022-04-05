0bc7d5099623cb4177639cfeee213e27cbfc1b72
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-04-05 17:00:33 +0200

    Revert "Update child_appointment_form.py"
    This reverts commit cb0bb305e0148cc9e9196225cd8b99bdcea89824.

--- flourish_child/forms/child_appointment_form.py
+++ flourish_child/forms/child_appointment_form.py
@@ -7,7 +7,7 @@ from edc_form_validators import FormValidatorMixin
 from ..models import Appointment
 
 
-class AppointmentForm(SiteModelFormMixin, FormValidatorMixin, AppointmentFormValidator,
+class AppointmentForm(AppointmentFormValidator, SiteModelFormMixin, FormValidatorMixin,
                       forms.ModelForm):
     """Note, the appointment is only changed, never added,
     through this form.
