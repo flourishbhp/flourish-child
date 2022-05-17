4ab01e2acf1763c5d0583fabaee2e9252c343a92
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-05-17 08:43:45 +0200

    Update signals.py

--- flourish_child/models/signals.py
+++ flourish_child/models/signals.py
@@ -221,7 +221,6 @@ def child_birth_on_post_save(sender, instance, raw, created, **kwargs):
 def clinician_notes_image_on_post_save(sender, instance, raw, created, **kwargs):
     if not raw and created:
         stamp_image(instance)
-        subject_identifier = instance.clinician_notes.subject_identifier
 
 
 def notification(subject_identifier,
