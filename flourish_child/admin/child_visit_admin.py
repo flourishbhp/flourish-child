5dd9c32aa131e3d30b8953b68c03f0b4a90c8c12
Itumeleng Mosweu <imosweuk@gmail.com>
2022-08-24 17:17:28 +0200

    add: visit admin mixins

--- flourish_child/admin/child_visit_admin.py
+++ flourish_child/admin/child_visit_admin.py
@@ -1,16 +1,16 @@
-from edc_visit_schedule.fieldsets import visit_schedule_fieldset_tuple
-
 from django.contrib import admin
 from django.urls.base import reverse
 from django.urls.exceptions import NoReverseMatch
 from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
+from edc_base.sites.admin import ModelAdminSiteMixin
 from edc_constants.constants import NO
+from edc_fieldsets import FieldsetsModelAdminMixin
 from edc_model_admin import (
     ModelAdminFormAutoNumberMixin, ModelAdminInstitutionMixin,
-    ModelAdminNextUrlRedirectMixin,
+    ModelAdminNextUrlRedirectMixin, ModelAdminAuditFieldsMixin,
     ModelAdminNextUrlRedirectError, ModelAdminReplaceLabelTextMixin)
 from edc_model_admin import audit_fieldset_tuple
-
+from edc_visit_schedule.fieldsets import visit_schedule_fieldset_tuple
 from edc_visit_tracking.modeladmin_mixins import VisitModelAdminMixin
 
 from ..admin_site import flourish_child_admin
@@ -21,7 +21,9 @@ from .exportaction_mixin import ExportActionMixin
 
 class ModelAdminMixin(ModelAdminNextUrlRedirectMixin, ModelAdminFormAutoNumberMixin,
                       ModelAdminRevisionMixin, ModelAdminReplaceLabelTextMixin,
-                      ModelAdminInstitutionMixin, ExportActionMixin):
+                      ModelAdminInstitutionMixin, ExportActionMixin,
+                      ModelAdminAuditFieldsMixin, FieldsetsModelAdminMixin,
+                      ModelAdminSiteMixin):
 
     list_per_page = 10
     date_hierarchy = 'modified'
