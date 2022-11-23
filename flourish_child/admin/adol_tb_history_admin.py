32af9e6dab789bd7ee0fe50c846f3388f605347f
Moses Chawawa <moseschawawa@gmail.com>
2022-11-23 12:24:30 +0200

    ADD: Added extra questions

--- flourish_child/admin/adol_tb_history_admin.py
+++ flourish_child/admin/adol_tb_history_admin.py
@@ -19,6 +19,8 @@ class TbHistoryAdolAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
                 'report_datetime',
                 'prior_tb_infec',
                 'history_of_tbt',
+                'reason_for_therapy',
+                'therapy_prescribed_age',
                 'tbt_completed',
                 'prior_tb_history',
                 'tb_diagnosis_type',
@@ -39,4 +41,6 @@ class TbHistoryAdolAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
                     'prior_treatmnt_history': admin.VERTICAL,
                     'tb_drugs_freq': admin.VERTICAL,
                     'iv_meds_used': admin.VERTICAL,
-                    'tb_treatmnt_completed': admin.VERTICAL}
+                    'tb_treatmnt_completed': admin.VERTICAL,
+                    'reason_for_therapy': admin.VERTICAL,
+                    'therapy_prescribed_age': admin.VERTICAL}
