bfc063b4d195fe56b5115dfca680c96c6b7b407c
Moses Chawawa <moseschawawa@gmail.com>
2022-11-24 14:16:44 +0200

    ADD: Formatting fields

--- flourish_child/admin/adol_tb_routines_screen_adol_admin.py
+++ flourish_child/admin/adol_tb_routines_screen_adol_admin.py
@@ -10,3 +10,31 @@ from .model_admin_mixins import ChildCrfModelAdminMixin
 @admin.register(TbRoutineScreenAdol, site=flourish_child_admin)
 class TbRoutineScreenAdolAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
     form = TbRoutineScreenAdolForm
+    
+    
+    fieldsets = (
+        (None, {
+            'fields': (
+                'child_visit',
+                'report_datetime',
+                'tb_health_visits',
+                'care_location',
+                'care_location_other',
+                'visit_reason',
+                'visit_reason_other',
+                'screening_questions',
+                'pos_screen',
+                'diagnostic_referral',)}
+         ), audit_fieldset_tuple)
+    
+    
+    radio_fields = {
+        'tb_health_visits': admin.VERTICAL,
+        'visit_reason': admin.VERTICAL,
+        'screening_questions': admin.VERTICAL,
+        'pos_screen': admin.VERTICAL,
+        'diagnostic_referral': admin.VERTICAL
+        
+    }
+    
+    filter_horizontal = ('care_location',)
