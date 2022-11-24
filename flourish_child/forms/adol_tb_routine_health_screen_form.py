a9c4a2d8e3bd08fbd8dbf9b3cdcfc4d97797e53c
Moses Chawawa <moseschawawa@gmail.com>
2022-11-24 14:17:01 +0200

    ADD: Validations

--- flourish_child/forms/adol_tb_routine_health_screen_form.py
+++ flourish_child/forms/adol_tb_routine_health_screen_form.py
@@ -2,10 +2,10 @@ from django import forms
 # from flourish_form_validation.form_validators import TbRoutineScreenAdolescentFormValidator
 from ..models import TbRoutineScreenAdol
 from .child_form_mixin import ChildModelFormMixin
-
+from flourish_child_validations.form_validators import TbScreeningDuringEncountersFormValidator
 
 class TbRoutineScreenAdolForm(ChildModelFormMixin):
-    # form_validator_cls = TbRoutineScreenAdolescentFormValidator
+    form_validator_cls = TbScreeningDuringEncountersFormValidator
 
     class Meta:
         model = TbRoutineScreenAdol
