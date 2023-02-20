e1347b147f7dc7d7ec444d38549a1f746c75ad82
salahdin <salahdinjemal@gmail.com>
2023-02-20 15:19:20 +0200

    prefill ChildTannerStaging to avoid inconstancy

--- flourish_child/forms/child_tanner_staging_form.py
+++ flourish_child/forms/child_tanner_staging_form.py
@@ -24,6 +24,16 @@ class ChildTannerStagingForm(ChildModelFormMixin, forms.ModelForm):
         kwargs['initial'] = initial
         super().__init__(*args, **kwargs)
 
+        prev_instance = ChildTannerStaging.objects.filter(
+            child_visit__appointment__subject_identifier=subject_identifier).order_by('-report_datetime').first()
+
+        if prev_instance:
+            self.initial['manarche_dt_avail'] = prev_instance.manarche_dt_avail
+            self.initial['menarche_dt'] = prev_instance.menarche_dt
+            self.initial['menarche_dt_est'] = prev_instance.menarche_dt_est
+            self.initial['male_gen_stage'] = prev_instance.male_gen_stage
+            self.initial['testclr_vol_measrd'] = prev_instance.testclr_vol_measrd
+
     class Meta:
         model = ChildTannerStaging
         fields = '__all__'
