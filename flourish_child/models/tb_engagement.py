9291873723583d10b893a00e832eefc12864da9e
nmunatsibw <mcturnergarai@gmail.com>
2023-04-03 17:00:50 +0200

    fix(forms): Add not eligible option

--- flourish_child/models/tb_engagement.py
+++ flourish_child/models/tb_engagement.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 from .child_crf_model_mixin import ChildCrfModelMixin
-from ..choices import DECLINE_REASON, YES_NO_NOT_LIGIBLE
+from ..choices import DECLINE_REASON, YES_NO_NOT_ELIGIBLE
 
 
 class TbAdolEngagement(ChildCrfModelMixin):
     interview_consent = models.CharField(
         verbose_name='Is the participant interested in participating in the interview?',
-        choices=YES_NO_NOT_LIGIBLE,
+        choices=YES_NO_NOT_ELIGIBLE,
         max_length=15, )
 
     interview_decline_reason = models.CharField(
