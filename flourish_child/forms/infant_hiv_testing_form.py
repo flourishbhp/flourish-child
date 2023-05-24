475a1a85a4e608f1ea393869de0c83b496a34a79
salahdin <salahdinjemal@gmail.com>
2023-05-24 12:37:17 +0200

    add validation

--- flourish_child/forms/infant_hiv_testing_form.py
+++ flourish_child/forms/infant_hiv_testing_form.py
@@ -1,12 +1,12 @@
 from django import forms
-# from flourish_form_validations.form_validators import HIVInfantTestingFormValidator
+from flourish_child_validations.form_validators import InfantHIVTestingFormValidator
 
 from ..models import InfantHIVTesting
 from flourish_caregiver.forms.form_mixins import SubjectModelFormMixin
 
 
 class InfantHIVTestingForm(SubjectModelFormMixin, forms.ModelForm):
-    # form_validator_cls = HIVInfantTestingFormValidator
+    form_validator_cls = InfantHIVTestingFormValidator
 
     class Meta:
         model = InfantHIVTesting
