882620baf6870b6cdcb9470f56cdc8418e8f6313
nmunatsibw <mcturnergarai@gmail.com>
2023-08-23 15:47:47 +0200

    "Add form validation in admin forms for enhanced data integrity"
    Updated Brief2SelfReportedAdmin and ChildCBCLAdmin forms in the flourish-child module to ensure enhanced data integrity. Imported json to use in the changeform_view() that checks if certain fields have been correctly filled before form submission. This will prevent the risk of incomplete or inconsistent entries. To achieve this, we added extra_context to the respective admin models.

--- flourish_child/admin/brief_2_self_reported_admin.py
+++ flourish_child/admin/brief_2_self_reported_admin.py
@@ -1,15 +1,16 @@
+import json
+
 from django.contrib import admin
 from edc_model_admin import audit_fieldset_tuple
 
+from .model_admin_mixins import ChildCrfModelAdminMixin
 from ..admin_site import flourish_child_admin
 from ..forms import Brief2SelfReportedForm
 from ..models import Brief2SelfReported
-from .model_admin_mixins import ChildCrfModelAdminMixin
 
 
 @admin.register(Brief2SelfReported, site=flourish_child_admin)
 class Brief2SelfReportedAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
-
     form = Brief2SelfReportedForm
 
     fieldsets = (
@@ -57,3 +58,14 @@ class Brief2SelfReportedAdmin(ChildCrfModelAdminMixin, admin.ModelAdmin):
                     'brief2_self_valid': admin.VERTICAL,
                     'brief2_self_impact_on_responses': admin.VERTICAL,
                     'brief2_self_invalid_reason': admin.VERTICAL, }
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['fields_to_check'] = json.dumps([
+            'brief2_self_interest',
+            'brief2_self_understanding',
+            'brief2_self_valid',
+            'brief2_self_invalid_reason',
+            'brief2_self_impact_on_responses',
+        ])
+        return super().changeform_view(request, object_id, form_url, extra_context)
