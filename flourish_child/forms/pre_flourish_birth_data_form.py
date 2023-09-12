6cdc1a7b75a9f07c4ae95554847f1531cba856ae
nmunatsibw <mcturnergarai@gmail.com>
2023-09-12 08:37:49 +0200

    Add FormValidatorMixin to PreFlourishBirthDataForm
    Integrated FormValidatorMixin into the PreFlourishBirthDataForm class to enhance form validation. This step is crucial in implementing DRY (Don't Repeat Yourself) principle, thereby increasing code efficiency and readability.

--- flourish_child/forms/pre_flourish_birth_data_form.py
+++ flourish_child/forms/pre_flourish_birth_data_form.py
@@ -1,11 +1,12 @@
 from django import forms
+from edc_form_validators import FormValidatorMixin
 
 from flourish_child.models.pre_flourish_birth_data import PreFlourishBirthData
 from flourish_child_validations.form_validators import \
     PreFlourishBirthDataFormValidator
 
 
-class PreFlourishBirthDataForm(forms.ModelForm):
+class PreFlourishBirthDataForm(FormValidatorMixin, forms.ModelForm):
     form_validator_cls = PreFlourishBirthDataFormValidator
 
     class Meta:
