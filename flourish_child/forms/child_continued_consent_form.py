441ae33d76c274c9c7c942337ceed9288d484ab6
Moses Chawawa <moseschawawa@gmail.com>
2023-10-05 14:11:56 +0200

    Prefill fix

--- flourish_child/forms/child_continued_consent_form.py
+++ flourish_child/forms/child_continued_consent_form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import model_to_dict
 from edc_base.sites import SiteModelFormMixin
 from edc_form_validators import FormValidatorMixin
 from flourish_child_validations.form_validators import ChildContinuedConsentFormValidator
@@ -16,19 +17,23 @@ class ChildContinuedConsentForm(SiteModelFormMixin, FormValidatorMixin, forms.Mo
 
     def __init__(self, *args, **kwargs):
         initial = kwargs.pop('initial', {})
-        subject_identifier = initial.get('subject_identifier')
-        version = initial.get('version')
-        if subject_identifier and version:
+        subject_identifier = initial.get('subject_identifier', None)
+
+        if subject_identifier:
             try:
-                child_assent = ChildAssent.objects.get(
-                    subject_identifier=subject_identifier,
-                    version=version)
+                child_assent = ChildAssent.objects.filter(
+                    subject_identifier=subject_identifier
+                    ).latest('consent_datetime')
             except ChildAssent.DoesNotExist:
                 raise forms.ValidationError(
                     'Child assent for this child is missing. Please complete form')
             else:
-                for key in self.base_fields.keys():
-                    if key not in ['subject_identifier', 'consent_datetime']:
+                # Fields that exist in both forms
+                fields = set(self.base_fields.keys()) \
+                    & set(model_to_dict(child_assent).keys())
+
+                for key in fields:
+                    if key not in ['subject_identifier', 'consent_datetime', ]:
                         initial[key] = getattr(child_assent, key)
         kwargs['initial'] = initial
         super().__init__(*args, **kwargs)
