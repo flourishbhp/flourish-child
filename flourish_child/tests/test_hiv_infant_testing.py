0894378c08cb2ad2f4fcaad20222d04cf6e6e806
nmunatsibw <mcturnergarai@gmail.com>
2023-10-18 16:00:23 +0200

    refactor(HIV infant testing conditions): Refactor hiv infant testing: Adjust age requirement and relevant conditions
    This commit simplifies the HIV infant testing conditions in both infant testing and child predicates. Initially, infant testing was required only under the age of 18 months. This constraint has been removed, so testing covers a broader age range. The test for infant testing not required after 18 months was also removed from the infant testing file. In the child predicates file, the condition 'child_age_in_months <= 18' was taken off. Additionally, "visit_code == '3000'" was added to the governing conditions for when HIV testing is required. This overall change provides a more comprehensive coverage for required tests in the study.

--- flourish_child/tests/test_hiv_infant_testing.py
+++ flourish_child/tests/test_hiv_infant_testing.py
@@ -59,7 +59,7 @@ class TestHivInfantTesting(TestCase):
             breastfeed_intent=YES,
             **self.options)
 
-    def test_hiv_infant_testing_required_18_months(self):
+    def test_hiv_infant_testing_required(self):
         self.preg_caregiver_child_consent_obj = mommy.make_recipe(
             'flourish_caregiver.caregiverchildconsent',
             subject_consent=self.preg_subject_consent,
@@ -123,71 +123,6 @@ class TestHivInfantTesting(TestCase):
             model='flourish_child.infanthivtesting',
             subject_identifier=self.preg_caregiver_child_consent_obj.subject_identifier,
             visit_code='2001').entry_status, REQUIRED)
-
-    def test_hiv_infant_testing_not_required_older_18_months(self):
-        preg_caregiver_child_consent_obj = mommy.make_recipe(
-            'flourish_caregiver.caregiverchildconsent',
-            subject_consent=self.preg_subject_consent,
-            gender=None,
-            first_name=None,
-            last_name=None,
-            identity=None,
-            confirm_identity=None,
-            study_child_identifier=None,
-            child_dob=None,
-            version='2')
-
-        self.preg_subject_identifier = self.preg_subject_consent.subject_identifier
-
-        mommy.make_recipe(
-            'flourish_caregiver.antenatalenrollment',
-            current_hiv_status=POS,
-            subject_identifier=self.preg_subject_consent.subject_identifier, )
-
-        mommy.make_recipe(
-            'flourish_caregiver.maternaldelivery',
-            subject_identifier=self.preg_subject_consent.subject_identifier,
-            delivery_datetime=get_utcnow() - relativedelta(years=1, months=7),
-            live_infants_to_register=1)
-
-        mommy.make_recipe(
-            'flourish_child.childbirth',
-            subject_identifier=preg_caregiver_child_consent_obj.subject_identifier,
-            dob=(get_utcnow() - relativedelta(years=1, months=7)).date(),
-            user_created='imosweu')
-
-        child_consent = ChildDummySubjectConsent.objects.get(
-            subject_identifier=preg_caregiver_child_consent_obj.subject_identifier,
-        )
-
-        child_consent.dob = (get_utcnow() - relativedelta(days=7)).date()
-        child_consent.save_base(raw=True)
-
-        mommy.make_recipe(
-            'flourish_child.childvisit',
-            appointment=Appointment.objects.get(
-                subject_identifier=preg_caregiver_child_consent_obj.subject_identifier,
-                visit_code='2000D'),
-            is_present=YES,
-            report_datetime=get_utcnow(),
-            reason=SCHEDULED)
-
-        self.assertEqual(OnScheduleChildCohortAQuarterly.objects.filter(
-            subject_identifier=preg_caregiver_child_consent_obj.subject_identifier,
-            schedule_name='child_a_quart_schedule1').count(), 1)
-
-        mommy.make_recipe(
-            'flourish_child.childvisit',
-            appointment=Appointment.objects.get(
-                visit_code='2001',
-                subject_identifier=preg_caregiver_child_consent_obj.subject_identifier),
-            report_datetime=get_utcnow(),
-            reason=SCHEDULED)
-
-        self.assertEqual(CrfMetadata.objects.get(
-            model='flourish_child.infanthivtesting',
-            subject_identifier=preg_caregiver_child_consent_obj.subject_identifier,
-            visit_code='2001').entry_status, NOT_REQUIRED)
 
     def test_hiv_infant_testing_required_not_required_prior_part(self):
         marternal_dataset_obj = mommy.make_recipe(
