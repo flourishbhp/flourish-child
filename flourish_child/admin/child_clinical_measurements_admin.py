e63667010ace3aa1524502d57b8022303e43e6f3
nmunatsibw <mcturnergarai@gmail.com>
2023-10-24 08:26:26 +0200

    :sparkles: feature(Child Clinical Measurements): Add skin fold completion check and refactor verbose names in models
    Extended Child Clinical Measurements model by adding new field "visit_skin_fold_messure" to check the completion of skin fold measurements at each visit. This was necessary to distinguish whether measurements were not taken vs not applicable. Verbose names and help texts for measurements fields were reformatted to increase readability and aid better understanding of each field. Changes were also reflected in the form validator and admin files to ensure proper validation and display of these new updates.

--- flourish_child/admin/child_clinical_measurements_admin.py
+++ flourish_child/admin/child_clinical_measurements_admin.py
@@ -37,6 +37,11 @@ class ChildClinicalMeasurementsAdmin(ChildCrfModelAdminMixin,
                 'child_hip_circ',
             ]}
          ),
+        ("Skin Folds Questions", {
+            'fields': [
+                'visit_skin_fold_messure',
+            ]}
+         ),
         ("Skin Folds Triceps", {
             'fields': [
                 'skin_folds_triceps',
@@ -61,7 +66,9 @@ class ChildClinicalMeasurementsAdmin(ChildCrfModelAdminMixin,
         audit_fieldset_tuple)
 
     radio_fields = {
-        'is_child_preg': admin.VERTICAL, }
+        'is_child_preg': admin.VERTICAL,
+        'visit_skin_fold_messure': admin.VERTICAL,
+    }
 
     conditional_fieldlists = {}
 
