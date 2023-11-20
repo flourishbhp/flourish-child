3de0ab6b1399d0e8f92359367ea107250214b8b7
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-11-20 10:40:13 +0200

    Prefill child_exposure_status

--- flourish_child/models/child_social_work_referral.py
+++ flourish_child/models/child_social_work_referral.py
@@ -1,8 +1,16 @@
 from .child_crf_model_mixin import ChildCrfModelMixin
 from flourish_caregiver.models.model_mixins import CaregiverSocialWorkReferralMixin
+from django.db import models
+from flourish_caregiver.choices import CAREGIVER_OR_CHILD
 
 
 class ChildSocialWorkReferral(ChildCrfModelMixin, CaregiverSocialWorkReferralMixin):
+
+    referral_for = models.CharField(
+        verbose_name='Referral For ',
+        max_length=10,
+        choices=CAREGIVER_OR_CHILD,
+        default='child')
 
     class Meta(ChildCrfModelMixin.Meta):
         app_label = 'flourish_child'
