341bacba1c29e7c43e0bde4199c31845252623e5
Ame Diphoko <diphokoame@gmail.com>
2023-12-06 16:39:08 +0200

    update: cbcl crfs, add numeric representation for the questions help text

--- flourish_child/models/child_cbcl_section1.py
+++ flourish_child/models/child_cbcl_section1.py
@@ -9,12 +9,14 @@ class ChildCBCLSection1(ChildCrfModelMixin):
     acts_young = models.CharField(
         verbose_name='Acts too young for his/her age',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(1)')
 
     unapproved_alc_intake = models.CharField(
         verbose_name='Drinks alcohol without parents\' approval',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(2)')
 
     alc_intake_desc = models.TextField(
         verbose_name='Drinks alcohol without parents\' approval (describe)',
@@ -23,37 +25,44 @@ class ChildCBCLSection1(ChildCrfModelMixin):
     argues_alot = models.CharField(
         verbose_name='Argues a lot',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(3)')
 
     fails_to_finish = models.CharField(
         verbose_name='Fails to finish things he/she starts',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(4)')
 
     enjoys_little = models.CharField(
         verbose_name='There is very little he/she enjoys',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(5)')
 
     bowel_incontinence = models.CharField(
         verbose_name='Bowel movements outside toilet',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(6)')
 
     bragging = models.CharField(
         verbose_name='Bragging, boasting',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(7)')
 
     attention_deficit = models.CharField(
         verbose_name='Can\'t concentrate, can\'t pay attention for long',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(8)')
 
     obssessive = models.CharField(
         verbose_name='Can\'t get his/her mind off certain thoughts; obsessions',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(9)')
 
     obssessive_desc = models.TextField(
         verbose_name='Can\'t get his/her mind off certain thoughts; obsessions (describe)',
@@ -62,103 +71,123 @@ class ChildCBCLSection1(ChildCrfModelMixin):
     hyperactive = models.CharField(
         verbose_name=' Can\'t sit still, restless, or hyperactive',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(10)')
 
     too_dependent = models.CharField(
         verbose_name='Clings to adults or too dependent',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(11)')
 
     feels_lonely = models.CharField(
         verbose_name='Complains of loneliness',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(12)')
 
     confused = models.CharField(
         verbose_name='Confused or seems to be in a fog',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(13)')
 
     cries_alot = models.CharField(
         verbose_name='Cries a lot',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(14)')
 
     animal_cruelty = models.CharField(
         verbose_name='Cruel to animals',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(15)')
 
     bullies_other = models.CharField(
         verbose_name='Cruelty, bullying, or meanness to others',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(16)')
 
     daydreams = models.CharField(
         verbose_name='Daydreams or gets lost in his/her thoughts',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(17)')
 
     self_harms = models.CharField(
         verbose_name='Deliberately harms self or attempts suicide',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(18)')
 
     demands_attention = models.CharField(
         verbose_name='Demands a lot of attention',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(19)')
 
     destroys_belongings = models.CharField(
         verbose_name='Destroys his/her own things',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(20)')
 
     destroys_othr_things = models.CharField(
         verbose_name='Destroys things belonging to his/her family or others',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(21)')
 
     disobedient_home = models.CharField(
         verbose_name='Disobedient at home',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(22)')
 
     disobedient_school = models.CharField(
         verbose_name='Disobedient at school',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(23)')
 
     eating_problems = models.CharField(
         verbose_name='Doesn\'t eat well',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(24)')
 
     unfitting = models.CharField(
         verbose_name='Doesn\'t get along with other kids',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(25)')
 
     unremorseful = models.CharField(
         verbose_name='Doesn\'t seem to feel guilty after misbehaving',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(26)')
 
     easily_jealous = models.CharField(
         verbose_name='Easily jealous',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(27)')
 
     breaks_rules = models.CharField(
         verbose_name='Breaks rules at home, school, or elsewhere',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(28)')
 
     fearful = models.CharField(
         verbose_name=('Fears certain animals, situations, or places, other than '
                       'school'),
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(29)')
 
     fearful_desc = models.TextField(
         verbose_name=('Fears certain animals, situations, or places, other than '
@@ -169,7 +198,8 @@ class ChildCBCLSection1(ChildCrfModelMixin):
     fears_school = models.CharField(
         verbose_name='Fears going to school',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(30)')
 
     class Meta(ChildCrfModelMixin.Meta):
         app_label = 'flourish_child'
--- flourish_child/models/child_cbcl_section4.py
+++ flourish_child/models/child_cbcl_section4.py
@@ -1,5 +1,4 @@
 from django.db import models
-from edc_base.model_fields import OtherCharField
 
 from .child_crf_model_mixin import ChildCrfModelMixin
 from .model_mixins.test_questions_mixin import TestQuestionMixin
@@ -10,22 +9,26 @@ class ChildCBCLSection4(ChildCrfModelMixin, TestQuestionMixin):
     stares_blankly = models.CharField(
         verbose_name='Stares blankly',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(80)')
 
     steals_at_home = models.CharField(
         verbose_name='Steals at home',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(81)')
 
     steals_elsewhere = models.CharField(
         verbose_name='Steals outside the home',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(82)')
 
     hoarding = models.CharField(
         verbose_name='Stores up too many things he/she doesn\'t need',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(83)')
 
     hoarding_desc = models.TextField(
         verbose_name='Stores up too many things he/she doesn\'t need (describe)',
@@ -35,7 +38,8 @@ class ChildCBCLSection4(ChildCrfModelMixin, TestQuestionMixin):
     strange_behavior = models.CharField(
         verbose_name='Strange behavior',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(84)')
 
     behavior_desc = models.TextField(
         verbose_name='Strange behavior (describe)',
@@ -45,7 +49,8 @@ class ChildCBCLSection4(ChildCrfModelMixin, TestQuestionMixin):
     strange_ideas = models.CharField(
         verbose_name='Strange ideas',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(85)')
 
     ideas_desc = models.TextField(
         verbose_name='Strange ideas (describe)',
@@ -55,37 +60,44 @@ class ChildCBCLSection4(ChildCrfModelMixin, TestQuestionMixin):
     irritable = models.CharField(
         verbose_name='Stubborn, sullen, or irritable',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(86)')
 
     sudden_mood_chng = models.CharField(
         verbose_name='Sudden changes in mood or feelings',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(87)')
 
     sulks_alot = models.CharField(
         verbose_name='Sulks a lot',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(88)')
 
     suspicious = models.CharField(
         verbose_name='Suspicious',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(89)')
 
     swearing = models.CharField(
         verbose_name='Swearing or obscene language',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(90)')
 
     self_harm_talks = models.CharField(
         verbose_name='Talks about killing self',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(91)')
 
     sleepwalk_talk = models.CharField(
         verbose_name='Talks or walks in sleep',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(92)')
 
     sleepwalk_desc = models.TextField(
         verbose_name='Talks or walks in sleep (describe)',
@@ -95,42 +107,50 @@ class ChildCBCLSection4(ChildCrfModelMixin, TestQuestionMixin):
     talks_alot = models.CharField(
         verbose_name='Talks too much',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(93)')
 
     teases_alot = models.CharField(
         verbose_name='Teases a lot',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(94)')
 
     hot_temper = models.CharField(
         verbose_name='Temper tantrum or hot temper ',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(95)')
 
     sex_thoughts = models.CharField(
         verbose_name='Thinks about sex too much',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(96)')
 
     threatens_people = models.CharField(
         verbose_name='Threatens people',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(97)')
 
     thumbsucking = models.CharField(
         verbose_name='Thumb-sucking',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(98)')
 
     smokes = models.CharField(
         verbose_name='Smokes, chews, sniffs tobacco or uses e-cigs',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(99)')
 
     trouble_sleeping = models.CharField(
         verbose_name='Trouble sleeping',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(100)')
 
     sleeping_desc = models.TextField(
         verbose_name='Trouble sleeping (describe)',
@@ -140,28 +160,33 @@ class ChildCBCLSection4(ChildCrfModelMixin, TestQuestionMixin):
     skips_school = models.CharField(
         verbose_name='Truancy, skips school',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(101)')
 
     underactive = models.CharField(
         verbose_name='Underactive, slow moving, or lacks energy',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(102)')
 
     unhappy = models.CharField(
         verbose_name='Unhappy, sad, or depressed',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(103)')
 
     unusually_loud = models.CharField(
         verbose_name='Unusually loud',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(104)')
 
     drug_usage = models.CharField(
         verbose_name='Uses drugs for nonmedical purposes (don\'t include alcohol or '
                      'tobacco)',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(105)')
 
     drug_usage_desc = models.TextField(
         verbose_name=(
@@ -173,40 +198,48 @@ class ChildCBCLSection4(ChildCrfModelMixin, TestQuestionMixin):
     vandalism = models.CharField(
         verbose_name='Vandalism',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(106)')
 
     daytime_wetting = models.CharField(
         verbose_name='Wets self during the day',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(107)')
 
     bedtime_wetting = models.CharField(
         verbose_name='Wets the bed',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(108)')
 
     whining = models.CharField(
         verbose_name='Whining',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(109)')
 
     gender_dissonant = models.CharField(
         verbose_name='Wishes to be of opposite sex',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(110)')
 
     withdrawn = models.CharField(
         verbose_name='Withdrawn, doesn\'t get invovled with others',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(111)')
 
     worries = models.CharField(
         verbose_name='Worries',
         choices=CBCL_SCALE,
-        max_length=10)
+        max_length=10,
+        help_text='(112)')
 
     other_problems = models.TextField(
-        verbose_name='Please write in any problems your child has that were not listed above',)
+        verbose_name='Please write in any problems your child has that were not listed above',
+        help_text='(113)')
 
     class Meta(ChildCrfModelMixin.Meta):
         app_label = 'flourish_child'
