c6f053a51d096a3ce6a9dc00a4baaf84ff5ddba6
nmunatsibw <mcturnergarai@gmail.com>
2024-04-08 15:43:39 +0200

    (Refactor): Add unknown option to response choices and updated list data
    This commit imports and implements expanded response options, to include "unknown", in several fields in the child_tb_screening.py model. To align with this change, list_data.py has been updated to include a "None" choice. Additionally, minor amendments have been made to child_tb_screening_admin.py to handle increased field variability and extend admin capabilities.
    
    Signed-off-by: nmunatsibw <mcturnergarai@gmail.com>

--- flourish_child/models/child_tb_screening.py
+++ flourish_child/models/child_tb_screening.py
@@ -1,42 +1,43 @@
 from django.db import models
 from edc_constants.choices import YES_NO
 
+from flourish_caregiver.choices import YES_NO_UKN_CHOICES
 from flourish_caregiver.models.model_mixins.flourish_tb_screening_mixin import \
     TBScreeningMixin
-from flourish_child.choices import TEST_RESULTS_CHOICES
+from flourish_child.choices import TEST_RESULTS_CHOICES, YES_NO_UNKNOWN
 from flourish_child.models.child_crf_model_mixin import ChildCrfModelMixin
 
 
 class ChildTBScreening(TBScreeningMixin, ChildCrfModelMixin):
     cough = models.CharField(
         verbose_name='Does your child currently have any cough?',
-        choices=YES_NO,
-        max_length=3)
+        choices=YES_NO_UKN_CHOICES,
+        max_length=20)
 
     fever = models.CharField(
         verbose_name='Does your child currently have a fever?',
-        choices=YES_NO,
-        max_length=3)
+        choices=YES_NO_UKN_CHOICES,
+        max_length=20)
 
     sweats = models.CharField(
         verbose_name='Is your child currently experiencing night sweats?',
-        choices=YES_NO,
+        choices=YES_NO_UNKNOWN,
         help_text='Night sweats is defined as waking up with your bed clothing soaked – '
                   'enough to require the bed clothing to be changed',
-        max_length=3)
+        max_length=20)
 
     weight_loss = models.CharField(
         verbose_name='Since the last time you spoke with FLOURISH staff, has your child '
                      'had any weight loss (or no weight gain for a child who is less '
                      'than 12 years of age)?',
-        choices=YES_NO,
-        max_length=3, )
+        choices=YES_NO_UKN_CHOICES,
+        max_length=20, )
 
     fatigue_or_reduced_playfulness = models.CharField(
         verbose_name='Does your child have fatigue or reduced playfulness that has '
                      'lasted ≥2 weeks?',
-        choices=YES_NO,
-        max_length=3, )
+        choices=YES_NO_UKN_CHOICES,
+        max_length=20, )
 
     stool_sample_results = models.CharField(
         verbose_name='Stool Sample Results',
@@ -45,20 +46,20 @@ class ChildTBScreening(TBScreeningMixin, ChildCrfModelMixin):
 
     child_diagnosed_with_tb = models.CharField(
         verbose_name='Was your child diagnosed with TB?',
-        choices=YES_NO,
-        max_length=3,
+        choices=YES_NO_UKN_CHOICES,
+        max_length=20,
         blank=True, null=True)
 
     child_on_tb_treatment = models.CharField(
         verbose_name='Was your child started on TB treatment?',
-        choices=YES_NO,
-        max_length=3,
+        choices=YES_NO_UKN_CHOICES,
+        max_length=20,
         blank=True, null=True)
 
     child_on_tb_preventive_therapy = models.CharField(
         verbose_name='Was your child started on TB preventative therapy?',
-        choices=YES_NO,
-        max_length=3,
+        choices=YES_NO_UKN_CHOICES,
+        max_length=20,
         blank=True, null=True)
 
     class Meta(ChildCrfModelMixin.Meta):
