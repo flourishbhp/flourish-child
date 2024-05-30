267712fabf64d52db8deb9d3f62ad17138ca8008
Ame Diphoko <diphokoame@gmail.com>
2024-05-30 12:01:39 +0200

    update: gestational age coding for birth data form

--- flourish_child/forms/birth_data_form.py
+++ flourish_child/forms/birth_data_form.py
@@ -1,9 +1,9 @@
-from django.apps import apps as django_apps
 from django import forms
 
 from flourish_child_validations.form_validators import BirthDataFormValidator
 from .child_form_mixin import ChildModelFormMixin
 from ..models import BirthData
+from ..helper_classes.utils import child_utils
 
 
 class BirthDataForm(ChildModelFormMixin):
@@ -16,48 +16,20 @@ class BirthDataForm(ChildModelFormMixin):
 
     def __init__(self, *args, **kwargs):
         super(BirthDataForm, self).__init__(*args, **kwargs)
-        self.subject_identifier = self.initial.get('subject_identifier')
-        if self.antenatal_enrolment_obj:
-            self.initial['gestational_age'] = self.antenatal_enrolment_obj.real_time_ga
-        elif self.get_ga_confirmed:
-            self.initial['gestational_age'] = self.get_ga_confirmed
-
-    antenatal_enrolment_model = 'flourish_caregiver.antenatalenrollment'
-
-    @property
-    def get_ga_confirmed(self):
-        if self.get_latest_ultrasound:
-            return self.get_latest_ultrasound.ga_confirmed
+        child_visit = self.child_visit_obj
+        subject_identifier = getattr(child_visit, 'subject_identifier', '')
+        gestational_age = self.initial.get('gestational_age', None)
+        if not gestational_age:
+            self.initial['gestational_age'] = child_utils.get_gestational_age(
+                subject_identifier)
 
     @property
-    def ultrasound_model_cls(self):
-        return django_apps.get_model('flourish_caregiver.ultrasound')
-
-    @property
-    def antenatal_enrolment_cls(self):
-        return django_apps.get_model(self.antenatal_enrolment_model)
-
-    @property
-    def get_latest_ultrasound(self):
+    def child_visit_obj(self):
         try:
-            return self.ultrasound_model_cls.objects.filter(
-                child_subject_identifier=self.subject_identifier,
-                maternal_visit__appointment__subject_identifier=(
-                    self.caregiver_subject_identifier)).order_by(
-                '-report_datetime').first()
-        except self.ultrasound_model_cls.DoesNotExist:
+            return self.visit_model.objects.get(
+                id=self.initial.get('child_visit'))
+        except self.visit_model.DoesNotExist:
             return None
-
-    @property
-    def antenatal_enrolment_obj(self):
-        try:
-            antenatal_enrolment_obj = self.antenatal_enrolment_cls.objects.get(
-                child_subject_identifier=self.subject_identifier,
-                subject_identifier=self.caregiver_subject_identifier)
-        except self.antenatal_enrolment_cls.DoesNotExist:
-            return None
-        else:
-            return antenatal_enrolment_obj
 
     class Meta:
         model = BirthData
--- flourish_child/helper_classes/utils.py
+++ flourish_child/helper_classes/utils.py
@@ -179,6 +179,24 @@ class ChildUtils:
         consent_obj = self.caregiver_subject_consent_obj(subject_identifier)
         return getattr(consent_obj, 'biological_caregiver', None) == YES
 
+    def get_gestational_age(self, subject_identifier):
+        """ Query the antenatal enrolment object, to get the child's current
+            gestational age using the child PID.
+        """
+        caregiver_identifier = child_utils.caregiver_subject_identifier(
+            subject_identifier)
+        antenatal_enrol = django_apps.get_model(
+            'flourish_caregiver.antenatalenrollment')
+
+        try:
+            antenatal_enrol_obj = antenatal_enrol.objects.get(
+                child_subject_identifier=subject_identifier,
+                subject_identifier=caregiver_identifier)
+        except antenatal_enrol.DoesNotExist:
+            return None
+        else:
+            return antenatal_enrol_obj.real_time_ga
+
 
 child_utils = ChildUtils()
 
