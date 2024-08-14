59550572f2a10c80d98ed9bfce7984838cdd8930
nmunatsibw <mcturnergarai@gmail.com>
2024-08-14 14:25:27 +0200

    (delete): Remove redundant previous test result fields
    Removed redundant previous test result ChoiceFields from PreviousResultsFormMixin. Updated ChildTBScreeningForm to dynamically generate these fields based on available previous instances. Improved logic in admin class to handle multiple previous instances and conditional fieldsets.
    
    Signed-off-by: nmunatsibw <mcturnergarai@gmail.com>

--- flourish_child/forms/child_tb_screening_form.py
+++ flourish_child/forms/child_tb_screening_form.py
@@ -9,37 +9,6 @@ from flourish_child_validations.form_validators.child_tb_screening_form_validato
 
 
 class PreviousResultsFormMixin(forms.Form):
-    chest_xray_results_previous = forms.ChoiceField(
-        choices=TEST_RESULTS_CHOICES,
-        required=False,
-        label="Previous Chest X-Ray Results",
-        widget=forms.RadioSelect)
-    sputum_sample_results_previous = forms.ChoiceField(
-        choices=TEST_RESULTS_CHOICES,
-        required=False,
-        label="Previous Sputum Sample Results",
-        widget=forms.RadioSelect)
-    stool_sample_results_previous = forms.ChoiceField(
-        choices=TEST_RESULTS_CHOICES,
-        required=False,
-        label="Previous Stool Sample Results",
-        widget=forms.RadioSelect)
-    urine_test_results_previous = forms.ChoiceField(
-        choices=TEST_RESULTS_CHOICES,
-        required=False,
-        label="Previous Urine Test Results",
-        widget=forms.RadioSelect)
-    skin_test_results_previous = forms.ChoiceField(
-        choices=TEST_RESULTS_CHOICES,
-        required=False,
-        label="Previous Skin Test Results",
-        widget=forms.RadioSelect)
-    blood_test_results_previous = forms.ChoiceField(
-        choices=TEST_RESULTS_CHOICES,
-        required=False,
-        label="Previous Blood Test Results",
-        widget=forms.RadioSelect)
-
     def clean(self):
         clean_data = super().clean()
         keys_before_child_visit = self.get_keys_before(clean_data, )
@@ -60,6 +29,32 @@ class PreviousResultsFormMixin(forms.Form):
 
 class ChildTBScreeningForm(PreviousResultsFormMixin, ChildModelFormMixin):
     form_validator_cls = ChildTBScreeningFormValidator
+    update_fields = [
+        'chest_xray_results',
+        'sputum_sample_results',
+        'stool_sample_results',
+        'urine_test_results',
+        'skin_test_results',
+        'blood_test_results',
+    ]
+
+    def convert_case(self, raw_string):
+        words = raw_string.split('_')
+        capitalized_words = [word.capitalize() for word in words]
+        return ' '.join(capitalized_words)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        previous_instances = self.previous_instances
+        for i, instance in enumerate(previous_instances):
+            for field in self.update_fields:
+                visit_code = instance.child_visit.visit_code
+                new_field_name = f"{visit_code}_{field}_previous"
+                self.fields[new_field_name] = forms.ChoiceField(
+                    choices=TEST_RESULTS_CHOICES,
+                    required=False,
+                    label=f"{self.convert_case(field)} for visit {visit_code}",
+                    widget=forms.RadioSelect)
 
     class Meta:
         model = ChildTBScreening
