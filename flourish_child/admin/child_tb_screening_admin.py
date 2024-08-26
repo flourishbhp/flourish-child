9131bc3b12dd599e33bb20d153a51906bd705a06
nmunatsibw <mcturnergarai@gmail.com>
2024-08-26 15:17:58 +0200

    (update): Refactor PreviousResultsFormMixin
    Refactored PreviousResultsFormMixin to PreviousFieldsForm for better structuring and clearer functionality. Updated admin module and forms accordingly, added custom template for child TB screening change form in Django admin.
    
    Signed-off-by: nmunatsibw <mcturnergarai@gmail.com>

--- flourish_child/admin/child_tb_screening_admin.py
+++ flourish_child/admin/child_tb_screening_admin.py
@@ -1,38 +1,16 @@
-from django import forms
 from django.contrib import admin
 from edc_constants.constants import PENDING
-from edc_fieldsets import Fieldsets, FieldsetsModelAdminMixin, Insert
+from edc_fieldsets import FieldsetsModelAdminMixin, Remove
 from edc_model_admin.model_admin_audit_fields_mixin import audit_fieldset_tuple
 
 from .model_admin_mixins import ChildCrfModelAdminMixin
 from ..admin_site import flourish_child_admin
-from ..choices import TEST_RESULTS_CHOICES
-from ..forms import ChildTBScreeningForm
+from ..forms import ChildTBScreeningForm, PreviousFieldsForm
 from ..helper_classes.utils import child_utils
 from ..models.child_tb_screening import ChildTBScreening
 
 
-class TbFieldsets(Fieldsets):
-    def _get_field_position(self, fields, insert_after):
-        try:
-            position = fields.index(insert_after) + 1
-        except ValueError:
-            position = 0
-        return position
-
-
 class PreviousResultsAdminMixin(FieldsetsModelAdminMixin, admin.ModelAdmin):
-
-    def get_previous_results_keys(self, request, obj=None, keys=None):
-        if keys is None:
-            keys = []
-        previous_instances = self.get_previous_instances(request)
-        for previous_instance in previous_instances:
-            if not obj and previous_instance:
-                for result in self.update_fields:
-                    if getattr(previous_instance, result) == PENDING:
-                        keys.append(result)
-        return keys
 
     def get_previous_instances(self, request):
         previous_instances = []
@@ -40,7 +18,8 @@ class PreviousResultsAdminMixin(FieldsetsModelAdminMixin, admin.ModelAdmin):
         while current_instance:
             if self.has_pending_results(current_instance):
                 previous_instances.append(current_instance)
-            ap = current_instance.child_visit.appointment
+            visit = getattr(current_instance, self.visit_attr)
+            ap = visit.appointment
             current_instance = self.get_previous_instance(request, ap)
         return previous_instances
 
@@ -51,61 +30,38 @@ class PreviousResultsAdminMixin(FieldsetsModelAdminMixin, admin.ModelAdmin):
         return False
 
     def add_view(self, request, form_url='', extra_context=None):
+        previous_instances = self.get_previous_instances(request)
+        update_fields = self.update_fields
         self.request = request
+        if previous_instances:
+            extra_context['previous_fields_form'] = PreviousFieldsForm(
+                visit_attr=self.visit_attr,
+                previous_instances=previous_instances, update_fields=update_fields)
+
         return self.changeform_view(request, None, form_url, extra_context)
 
     def save_model(self, request, obj, form, change):
         previous_instances = self.get_previous_instances(request)
         if previous_instances:
-            for previous_instance in previous_instances:
-                changed = False
-                for field in form.cleaned_data:
-                    if field.endswith('_previous'):
-                        original_field_name = field[:-9]
-                        previous_value = form.cleaned_data[field]
-                        if previous_value != getattr(
-                                previous_instance, original_field_name):
-                            setattr(previous_instance,
-                                    original_field_name, previous_value)
-                            changed = True
-                if changed:
+            previous_fields_form = PreviousFieldsForm(
+                previous_instances=previous_instances,
+                update_fields=self.update_fields,
+                data=request.POST,
+                visit_attr=self.visit_attr,
+            )
+            if previous_fields_form.is_valid():
+                for previous_instance in previous_instances:
+                    for result in self.update_fields:
+                        visit = getattr(previous_instance, self.visit_attr)
+                        field_name = f'{visit.visit_code}_{result}'
+                        if field_name in previous_fields_form.cleaned_data:
+                            setattr(
+                                previous_instance,
+                                result,
+                                previous_fields_form.cleaned_data[field_name]
+                            )
                     previous_instance.save()
         super().save_model(request, obj, form, change)
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj=obj, **kwargs)
-        previous_instances = self.get_previous_instances(request)
-
-        for i, instance in enumerate(previous_instances):
-            for field in self.update_fields:
-                visit_code = instance.child_visit.visit_code
-                new_field_name = f"{visit_code}_{field}_previous"
-
-                form.base_fields[new_field_name] = forms.ChoiceField(
-                    choices=TEST_RESULTS_CHOICES,
-                    required=False,
-                    label=f"{self.convert_case(field)} for visit {visit_code}",
-                    widget=forms.RadioSelect)
-
-        return form
-
-    def convert_case(self, raw_string):
-        words = raw_string.split('_')
-        capitalized_words = [word.capitalize() for word in words]
-        return ' '.join(capitalized_words)
-
-    def remove_unused_section(self, fieldsets):
-        fieldsets_list = list(fieldsets)
-
-        for index, fieldset in enumerate(fieldsets_list):
-            if fieldset[0] == 'Previous Test Results':
-                if len(fieldset[1]['fields']) == 0:
-                    del fieldsets_list[index]
-                    break
-
-        fieldsets = tuple(fieldsets_list)
-
-        return fieldsets
 
     update_fields = [
         'chest_xray_results',
@@ -122,10 +78,9 @@ class ChildTBScreeningAdmin(ChildCrfModelAdminMixin, PreviousResultsAdminMixin,
                             admin.ModelAdmin):
     form = ChildTBScreeningForm
 
+    visit_attr = 'child_visit'
+
     fieldsets = (
-        ('Previous Test Results', {
-            'fields': []
-        }),
         (None, {
             'fields': [
                 'child_visit',
@@ -185,37 +140,17 @@ class ChildTBScreeningAdmin(ChildCrfModelAdminMixin, PreviousResultsAdminMixin,
 
     filter_horizontal = ('tb_tests',)
 
-    def get_keys(self, request, obj=None):
-        keys = []
+    def get_key(self, request, obj=None):
         try:
             visit_obj = self.visit_model.objects.get(id=request.GET.get('child_visit'))
         except self.visit_model.DoesNotExist:
-            pass
+            return None
         else:
             subject_identifier = visit_obj.subject_identifier
             child_age = child_utils.child_age(subject_identifier,
                                               visit_obj.report_datetime)
-            if child_age and child_age < 12:
-                keys.append('not_adol')
-        return self.get_previous_results_keys(request, obj, keys)
+            return 'not_adol' if child_age < 12 else None
 
-    @property
-    def conditional_fieldlists(self):
-        conditional_fieldlists = {
-            'not_adol': Insert(('fatigue_or_reduced_playfulness',),
-                               after='weight_loss_duration'),
-        }
-        return self.get_previous_results_conditional_fieldlists(conditional_fieldlists)
-
-    def get_fieldsets(self, request, obj=None):
-        fieldsets = super().get_fieldsets(request, obj=obj)
-        fieldsets = self.add_or_remove_fieldsets(request, obj, fieldsets)
-        return fieldsets
-
-    def add_or_remove_fieldsets(self, request, obj, fieldsets):
-        keys = self.get_keys(request, obj)
-        for key in keys:
-            fieldset = self.conditional_fieldlists.get(key)
-            if fieldset:
-                fieldsets = self.add_fieldsets(fieldsets, fieldset)
-        return fieldsets
+    conditional_fieldlists = {
+        'not_adol': Remove('fatigue_or_reduced_playfulness', ),
+    }
--- flourish_child/forms/child_tb_screening_form.py
+++ flourish_child/forms/child_tb_screening_form.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.core.exceptions import ValidationError
+from edc_constants.constants import PENDING
 
 from flourish_child.choices import TEST_RESULTS_CHOICES
 from flourish_child.forms.child_form_mixin import ChildModelFormMixin
@@ -8,53 +8,37 @@ from flourish_child_validations.form_validators.child_tb_screening_form_validato
     ChildTBScreeningFormValidator
 
 
-class PreviousResultsFormMixin(forms.Form):
-    def clean(self):
-        clean_data = super().clean()
-        keys_before_child_visit = self.get_keys_before(clean_data, )
-        for field in keys_before_child_visit:
-            field_value = clean_data.get(field)
-            if field_value == '':
-                raise ValidationError({field: 'This field is required.'})
-
-    def get_keys_before(self, dict):
-        key_stop = "child_visit"
-        return_list = []
-        for key in dict:
-            if key == key_stop:
-                break
-            return_list.append(key)
-        return return_list
-
-
-class ChildTBScreeningForm(PreviousResultsFormMixin, ChildModelFormMixin):
-    form_validator_cls = ChildTBScreeningFormValidator
-    update_fields = [
-        'chest_xray_results',
-        'sputum_sample_results',
-        'stool_sample_results',
-        'urine_test_results',
-        'skin_test_results',
-        'blood_test_results',
-    ]
+class PreviousFieldsForm(forms.Form):
 
     def convert_case(self, raw_string):
         words = raw_string.split('_')
         capitalized_words = [word.capitalize() for word in words]
         return ' '.join(capitalized_words)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, previous_instances=None, update_fields=None, visit_attr=None,
+                 *args, **kwargs):
         super().__init__(*args, **kwargs)
-        previous_instances = self.previous_instances
-        for i, instance in enumerate(previous_instances):
-            for field in self.update_fields:
-                visit_code = instance.child_visit.visit_code
-                new_field_name = f"{visit_code}_{field}_previous"
-                self.fields[new_field_name] = forms.ChoiceField(
-                    choices=TEST_RESULTS_CHOICES,
-                    required=False,
-                    label=f"{self.convert_case(field)} for visit {visit_code}",
-                    widget=forms.RadioSelect)
+        if previous_instances:
+            for previous_instance in previous_instances:
+                visit = getattr(previous_instance, visit_attr)
+                visit_code = visit.visit_code
+                for result in update_fields:
+                    if getattr(previous_instance, result) == PENDING:
+                        self.fields[f'{visit_code}_{result}'] = forms.ChoiceField(
+                            choices=TEST_RESULTS_CHOICES,
+                            required=False,
+                            label=f"{self.convert_case(result)} for visit "
+                                  f"{visit_code}",
+                            widget=forms.RadioSelect,
+                            initial=getattr(previous_instance, result)
+                        )
+
+        fields_order = list(self.fields.keys())
+        self.order_fields(fields_order)
+
+
+class ChildTBScreeningForm(ChildModelFormMixin):
+    form_validator_cls = ChildTBScreeningFormValidator
 
     class Meta:
         model = ChildTBScreening
