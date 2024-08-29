2a0a0b48db99ffaffda6196f353b999e6ba6bdcb
nmunatsibw <mcturnergarai@gmail.com>
2024-08-29 09:02:08 +0200

    (fix): Correct 'None' constant usage in list_data.py
    The 'None' constant was incorrectly used as a string. It has been corrected to use the NONE constant variable instead. This ensures consistency and avoids potential issues with string-based comparisons.
    
    Signed-off-by: nmunatsibw <mcturnergarai@gmail.com>

--- flourish_child/list_data.py
+++ flourish_child/list_data.py
@@ -286,7 +286,7 @@ list_data = {
         ('urine_test', 'Urine test (LAM)'),
         ('skin_test', 'Skin test (TST/Mantoux)'),
         ('blood_test', 'Blood test (quantiferon)'),
-        ('None', 'None'),
+        (NONE, 'None'),
         (OTHER, 'other')
     ],
     'flourish_child.childhivtestvisits': [
