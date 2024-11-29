bc358a4ae54d035cc7e61dd72bba888d2ec9dfcd
Ame Diphoko <diphokoame@gmail.com>
2024-11-29 15:40:30 +0200

    update: child continued consents conditions, and rendering

--- flourish_child/forms/child_visit_form.py
+++ flourish_child/forms/child_visit_form.py
@@ -6,7 +6,6 @@ from edc_base.utils import age, get_utcnow
 from edc_form_validators import FormValidatorMixin
 
 from flourish_child_validations.form_validators import ChildVisitFormValidator
-from ..action_items import CHILDASSENT_ACTION, CHILDCONTINUEDCONSENT_STUDY_ACTION
 from ..helper_classes.utils import child_utils
 from ..models import ChildVisit
 
@@ -31,7 +30,6 @@ class ChildVisitForm(
             self.validate_incomplete_required_model(
                 subject_identifier=self.subject_identifier,
                 model='flourish_child.childcontinuedconsent',
-                action_name=CHILDCONTINUEDCONSENT_STUDY_ACTION,
                 msg=('Participant is 18 years of age, cannot edit visit until '
                      'participant has given their continued consent for participation.'))
 
@@ -43,12 +41,11 @@ class ChildVisitForm(
                 self.validate_incomplete_required_model(
                     subject_identifier=self.subject_identifier,
                     model='flourish_child.childassent',
-                    action_name=CHILDASSENT_ACTION,
                     msg=('Participant is older than 7 years, please complete the child'
                          ' assent form before continuing with the visits.'))
 
     def validate_incomplete_required_model(
-            self, subject_identifier=None, model=None, action_name=None, msg=None):
+            self, subject_identifier=None, model=None, msg=None):
         model_cls = django_apps.get_model(model)
 
         consent_version = child_utils.consent_version(
